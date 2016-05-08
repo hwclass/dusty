ff4704d5cfb91c4f0a6290be98ab72d5fb65ff56
hwclass <harderworking.class@gmail.com>
2016-05-08 21:20:01 +0300

    updated readme and updated the package file descriptions modifiying with jsnext and browser props

--- tools/build.js
+++ tools/build.js
@@ -31,12 +31,12 @@ const bundles = [
   {
     format: 'umd', ext: '.js', plugins: [],
     babelPresets: ['es2015-rollup', 'stage-1'], babelPlugins: [],
-    moduleName: 'my-library'
+    moduleName: 'dusty-1.3.2'
   },
   {
     format: 'umd', ext: '.min.js', plugins: [uglify()],
     babelPresets: ['es2015-rollup', 'stage-1'], babelPlugins: [],
-    moduleName: 'my-library', minify: true
+    moduleName: 'dusty-1.3.2', minify: true
   }
 ];
 
@@ -47,34 +47,39 @@ promise = promise.then(() => del(['dist/*']));
 
 // Compile source code into a distributable format with Babel and Rollup
 for (const config of bundles) {
-  promise = promise.then(() => rollup.rollup({
-    entry: 'src/main.js',
-    external: Object.keys(pkg.dependencies),
-    plugins: [
-      babel({
-        babelrc: false,
-        exclude: 'node_modules/**',
-        presets: config.babelPresets,
-        plugins: config.babelPlugins,
-      })
-    ].concat(config.plugins),
-  }).then(bundle => bundle.write({
-    dest: `dist/${config.moduleName || 'main'}${config.ext}`,
-    format: config.format,
-    sourceMap: !config.minify,
-    moduleName: config.moduleName,
-  })));
+  if (typeof config !== null) {
+    console.log(111);
+    promise = promise.then(() => rollup.rollup({
+      entry: '../src/main.js',
+      external: Object.keys(pkg.dependencies),
+      plugins: [
+        babel({
+          babelrc: false,
+          exclude: 'node_modules/**',
+          presets: config.babelPresets,
+          plugins: config.babelPlugins,
+        })
+      ].concat(config.plugins),
+    }).then(bundle => bundle.write({
+      dest: `dist/${config.moduleName || 'main'}${config.ext}`,
+      format: config.format,
+      sourceMap: !config.minify,
+      moduleName: config.moduleName,
+    })));
+  }
 }
 
 // Copy package.json and LICENSE.txt
 promise = promise.then(() => {
+  console.log(222);
   delete pkg.private;
   delete pkg.devDependencies;
   delete pkg.scripts;
   delete pkg.eslintConfig;
   delete pkg.babel;
-  fs.writeFileSync('dist/package.json', JSON.stringify(pkg, null, '  '), 'utf-8');
-  fs.writeFileSync('dist/LICENSE.txt', fs.readFileSync('LICENSE.txt', 'utf-8'), 'utf-8');
+  fs.writeFileSync('../dist/package.json', JSON.stringify(pkg, null, '  '), 'utf-8');
+  fs.writeFileSync('../dist/LICENSE.txt', fs.readFileSync('LICENSE.txt', 'utf-8'), 'utf-8');
+  console.log(333);
 });
 
 promise.catch(err => console.error(err.stack)); // eslint-disable-line no-console
